de8d2b47cd574fcbc317d8072935663291621f86
mstale <mstale20@gmail.com>
2021-06-29 12:40:52 +0530

    Added requierd import statements

--- src/main/java/com/mstale/demo/DemoApplication.java
+++ src/main/java/com/mstale/demo/DemoApplication.java
@@ -2,7 +2,10 @@ package com.mstale.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
@@ -11,9 +14,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 	@GetMapping("/")
-	public String hello(@RequestParam(value = "name", defaultValue = "Mr.Stark!") String name) {
+	public String hello(@RequestParam(value = "name", defaultValue = "Mr.Tale!") String name) {
 		return String.format("Welcome %s!", name);
 	}
-
 }
